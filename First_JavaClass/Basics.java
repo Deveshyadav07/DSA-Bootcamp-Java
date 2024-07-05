990427566a3afe2e15fb67e81521e2c89379fec6
Deveshyadav07 <deveshyadav55655@gmail.com>
2024-07-05 09:37:45 +0530

    Learn about basics of java:

---
+++ First_JavaClass/Basics.java
@@ -0,0 +1,13 @@
+public class Basics {
+    public static void main(String[] args) {
+
+        // PRINT HELLO WORLD!
+        System.out.println("HELLO WORLD!");
+
+        int a = 10;
+        if (a == 10) {
+        System.out.println("Hello World");
+        }
+        
+    }
+}
