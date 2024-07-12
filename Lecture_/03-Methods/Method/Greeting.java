dea41f66424f30dc02bdf36d426e156b1afa5c3e
Deveshyadav07 <deveshyadav55655@gmail.com>
2024-07-12 12:11:22 +0530

    learn about method/function

---
+++ Lecture_/03-Methods/Method/Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+  public static void main(String[] args) {
+    greeting();
+    greeting();
+  }
+
+  // to reuse code
+  static void greeting() {
+    System.out.println("Hello Devesh !");
+  }
+}
